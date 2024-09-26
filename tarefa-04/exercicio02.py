8b46d9644dc6f2b456098c8e6ba9b12715ba7274
Otavio <otavio.sudano@email.com>
2024-09-26 09:13:38 -0300

    feat: add tarefa_04

---
+++ tarefa-04/exercicio02.py
@@ -0,0 +1,9 @@
+print("==============================")
+valor = int(input("Informe a primeira nota: "))
+resto = valor % 3
+
+if resto == 0:
+    print("O número é divisivel por 3")
+else:
+    print("O número não é divisivel por 3")
+print("==============================")
